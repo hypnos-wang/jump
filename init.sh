39b59efc8eb12d1d5345e3ae31e9ea9d464c3dd0
hypnos <3124763391@qq.com>
2022-08-30 16:13:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,33 +2,29 @@
 
 # download files
 mkdir -p /etc/caddy /usr/share/caddy
-URL="https://raw.githubusercontent.com/hypnos-wang/jump/main/files"
-wget -qO /etc/caddy/caddy.conf $URL/caddy.conf
-wget -qO /usr/share/caddy/index.html $URL/index.html
-wget -qO /jump.json $URL/jump.json
-wget -qO /xp $URL/xp
+# URL="https://raw.githubusercontent.com/hypnos-wang/jump/main/files"
+# wget -qO /etc/caddy/caddy.conf $URL/caddy.conf
+# wget -qO /usr/share/caddy/index.html $URL/index.html
+# wget -qO /jump.json $URL/jump.json
+# wget -qO /xp $URL/xp
 
 # set up variables
 PORT=443
 if [ "$UUID" = "" ]; then
   UUID=$(cat /proc/sys/kernel/random/uuid)
 fi
-PATH="/less"
+WSPATH="/less"
 echo "UUID: $UUID"
-echo "PATH: $PATH"
+echo "PATH: $WSPATH"
 
-if [ -e /jump.json ]; then
-  cat /jump.json
-fi
+# replace variables
+sed -e "s/\$PATH/$WSPATH/g" -e "s/\$PORT/$PORT/g" /etc/caddy/caddy.conf
+sed -e "s/\$UUID/$UUID/g" -e "s/\$PATH/$WSPATH/g" /jump.json
 
-# # replace variables
-# sed -e "s/\$PATH/$PATH/g" -e "s/\$PORT/$PORT/g" /etc/caddy/caddy.conf
-# sed -e "s/\$UUID/$UUID/g" -e "s/\$PATH/$PATH/g" /jump.json
+# run xp
+chmod +x /xp
+/xp -config /jump.json &
 
-# # run xp
-# chmod +x /xp
-# /xp -config /jump.json &
-
-# # run caddy
-# caddy run --config /etc/caddy/caddy.conf --adapter caddyfile
+# run caddy
+caddy run --config /etc/caddy/caddy.conf --adapter caddyfile
 

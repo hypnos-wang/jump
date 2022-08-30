2357d7cafca1b28679b5ebc311ca0ed4c02d5698
hypnos <3124763391@qq.com>
2022-08-30 15:59:44 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,24 +7,28 @@ wget -qO /etc/caddy/caddy.conf $URL/caddy.conf
 wget -qO /usr/share/caddy/index.html $URL/index.html
 wget -qO /jump.json $URL/jump.json
 wget -qO /xp $URL/xp
-[ ! -e /xp ] && echo "/xp does not exist." && return 1
 
 # set up variables
 PORT=443
-UUID=$(cat /proc/sys/kernel/random/uuid)
+if [ "$UUID" = "" ]; then
+  UUID=$(cat /proc/sys/kernel/random/uuid)
+fi
 PATH="less"
 echo "UUID: $UUID"
 echo "PATH: $PATH"
-echo "TEST: $TEST"
 
-# replace variables
-sed -e "s/\$PATH/$PATH/g" -e "s/\$PORT/$PORT/g" /etc/caddy/caddy.conf
-sed -e "s/\$UUID/$UUID/g" -e "s/\$PATH/$PATH/g" /jump.json
+if [ -e /xp ]; then
+  echo "yes"
+fi
 
-# run xp
-chmod +x /xp
-/xp -config /jump.json &
+# # replace variables
+# sed -e "s/\$PATH/$PATH/g" -e "s/\$PORT/$PORT/g" /etc/caddy/caddy.conf
+# sed -e "s/\$UUID/$UUID/g" -e "s/\$PATH/$PATH/g" /jump.json
 
-# run caddy
-caddy run --config /etc/caddy/caddy.conf --adapter caddyfile
+# # run xp
+# chmod +x /xp
+# /xp -config /jump.json &
+
+# # run caddy
+# caddy run --config /etc/caddy/caddy.conf --adapter caddyfile
 

999ea12f89b0d867b0ab426641144a17114a7d81
hugo-on <101410426+hugo-on@users.noreply.github.com>
2022-09-21 22:32:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,23 +1,76 @@
 #!/bin/sh
 
 # set up variables
-# PORT=443
+PORT=
+UUID=""
 if [ "$UUID" = "" ]; then
   UUID=$(cat /proc/sys/kernel/random/uuid)
 fi
-WSPATH="less"
-echo "UUID: $UUID"
-echo "PATH: $WSPATH"
-echo "PORT: $PORT"
+WSPATH="/less"
 
-# replace variables
-sed -i -e "s/\$PATH/\/$WSPATH/g" -e "s/\$PORT/$PORT/g" /etc/caddy/Caddyfile
-sed -i -e "s/\$UUID/$UUID/g" -e "s/\$PATH/\/$WSPATH/g" /jump.json
+mkdir -p /etc/caddy /usr/share/caddy
 
-# run xp
+# caddy file
+cat > /etc/caddy/Caddyfile << EOF
+:$PORT {
+	root * /usr/share/caddy
+	file_server
+
+	reverse_proxy $WSPATH {
+		to unix//etc/caddy/less
+	}
+}
+EOF
+
+# json file
+cat > /xp.json << EOF
+{
+    "inbounds": 
+    [
+        {
+            "listen": "/etc/caddy/less",
+            "protocol": "vless",
+            "settings": {
+				"clients": [
+					{"id": "$UUID"}
+				],
+				"decryption": "none"
+			},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "$WSPATH"}}
+        }
+    ],
+    
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}}
+    ] 
+}
+EOF
+
+# web index page
+cat > /usr/share/caddy/index.html << EOF
+<html>
+  <head></head>
+  <title>Authentication required</title>
+
+  <body>
+    <h1>Permission denied.</h1>
+  </body>
+</html>
+EOF
+
+# run xp, this is the only file that we need to download manually.
+if ! test -x /xp; then
+	wget -q -O /xp https://github.com/hugo-on/jump/raw/main/files/xp
+fi
 chmod +x /xp
 /xp -config /jump.json &
 
 # run caddy
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+
+# print messages
+echo "UUID: $UUID"
+echo "PATH: $WSPATH"
+echo "PORT: $PORT"
 
